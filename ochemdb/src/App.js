42f3f87e5d3a5d2ec99f62560ca41fa17188ca9b
Aadi <63931334+Aadi-1@users.noreply.github.com>
2025-02-01 12:21:13 -0800

    fixed BrowserRouter hopefully

--- ochemdb/src/App.js
+++ ochemdb/src/App.js
@@ -4,13 +4,13 @@ import About from "./pages/About";
 import Instructions from "./pages/Instructions";
 import RequestChemical from "./pages/RequestChemical";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { useCart } from "./CartContext";
-import table from "./pages/table.js";
+import Table from "./pages/table.js";
 
 function App() {
   return (
-    <Router>
+    <>
       <NavBar />
       <div className="container">
         <Routes>
@@ -18,10 +18,10 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/instructions" element={<Instructions />} />
           <Route path="/request" element={<RequestChemical />} />
-          <Route path="/table" element={<table />} />
+          <Route path="/table" element={<Table />} />
         </Routes>
       </div>
-    </Router>
+    </>
   );
 }
 
--- ochemdb/src/index.js
+++ ochemdb/src/index.js
@@ -3,21 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {
-  BrowserRouter,
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./CartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CartProvider>
-      <BrowserRouter basename="/">
+    <BrowserRouter basename="/">
+      <CartProvider>
         <App />
-      </BrowserRouter>
-    </CartProvider>
+      </CartProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
